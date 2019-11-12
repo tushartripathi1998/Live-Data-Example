04daba583bf63bd8daf60692a02dc02d97138c33
tushartripathi1998 <thetushartripathi1998@gmail.com>
2019-11-12 07:12:56 +0530

    first commit

---
+++ app/src/main/java/com/example/livedatanew/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.livedatanew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.btn_enter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RoomActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
